3e6f1f2eda678e7db11cb89814db8f0bd84e858a
barzan saeedpour <barzansaeedpour@gmail.com>
2022-11-28 09:31:03 +0000

    Finish crispy forms

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,11 @@ from blog.models import Tag, Post, Comment
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
+
+# class CommentAdmin(admin.ModelAdmin):
+#     fields = ('creator','content','content_type')
+#     list_display = ('creator','content','content_type')
+
 # Register your models here.
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
