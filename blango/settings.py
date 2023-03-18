3c914bd40efd88ee855a2a555cb1d64b7f494437
barzan saeedpour <barzansaeedpour@gmail.com>
2023-03-18 09:37:16 +0000

    Finish django Allauth not working yet

--- blango/settings.py
+++ blango/settings.py
@@ -52,16 +52,28 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      "django.contrib.sites",
       'django.contrib.staticfiles',
       ####################################
       'crispy_forms',
       'crispy_bootstrap5',
       "debug_toolbar",
       ####################################
+      "allauth", 
+      "allauth.account", 
+      "allauth.socialaccount", 
+      "allauth.socialaccount.providers.google",
+      ####################################
       'blog',
       "blango_auth", 
       
   ]
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
+
   INTERNAL_IPS = ["192.168.10.226"]
 
   CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
